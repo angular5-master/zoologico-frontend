216ee0f1696cd89f6aa736ecb1209f38f6c2a62d
albracu <yo@alfredobravocuero.co>
2017-12-29 09:11:38 -0500

    Creacion de Modelos en Angular

---
+++ src/app/models/animal.ts
@@ -0,0 +1,10 @@
+export class Animal{
+    constructor(
+        public _id: string,
+        public name: string,
+        public description: string,
+        public year: number,
+        public image: string,
+        public user: string
+    ){}
+}
