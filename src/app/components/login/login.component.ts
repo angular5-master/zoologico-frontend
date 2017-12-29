c1391cfad842a6ed79486c63463176ca241c5032
albracu <yo@alfredobravocuero.co>
2017-12-29 12:46:21 -0500

    Autenticacion y login de usuario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,24 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-
+import { User } from '../../models/user';
+import { GLOBAL } from '../../services/global';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  providers: [UserService]
 })
 
 export class LoginComponent implements OnInit {
   public title: string;
+  public user: User;
+  public identity;
+  public token;
+  public status: any;
 
   constructor(
       private _route: ActivatedRoute,
-      private _router: Router
+      private _router: Router,
+      private _userService: UserService
   ){
       this.title = 'Identificacion';
+      this.user = new User('', '', '', '', '', 'ROLE_USER','');
   }
 
   ngOnInit(){
       console.log('login.component cargado!!');
+  }
+
+  onSubmit(){
+      // Loguear al usuario y conseguir el Objeto
+      this._userService.signup(this.user).subscribe(
+          response => {
+            this.identity = response.user;
+
+            if(!this.identity || !this.identity._id){
+                alert('El usuario no se ha logueado correctamente');
+            }else{
+                this.identity.password = '';
+
+                // Mostrar el Identity
+                console.log(this.identity);
+
+                // Conseguir el token
+                this._userService.signup(this.user, true).subscribe(
+                    response => {
+                      this.token = response.token;
+          
+                      if(this.token.length <= 0){
+                        alert('El token no se ha generado');
+                      }else{
+                        // Mostrar el Token
+                        console.log(this.token);
+                        this.status = 'success'
+                      }
+                    },
+                    error => {
+                      console.log(<any>error);
+                    }
+                );
+            }
+          },
+          error => {
+            var errorMessage = <any>error;
+
+            if(errorMessage != null){
+                var body = JSON.parse(error._body);
+                this.status = 'error';
+            }
+          }
+      );
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,12 +11,25 @@ export class UserService{
     constructor(private _http: Http){
         this.url = GLOBAL.url;
     }
-
+    // Metodo para el servicio de Registro de usuario
     register(user_to_register){
         let params = JSON.stringify(user_to_register);
         let headers = new Headers({'Content-Type': 'application/json'});
 
         return this._http.post(this.url+'register', params, {headers: headers})
+                         .map(res => res.json());
+    }
+
+    // Metodo para el servicio de Login de usuario
+    signup(user_to_login, gettoken = null){
+        if(gettoken != null){
+            user_to_login.gettoken = gettoken;
+        }
+
+        let params = JSON.stringify(user_to_login);
+        let headers = new Headers({'Content-Type': 'application/json'});
+
+        return this._http.post(this.url+'login', params, {headers: headers})
                          .map(res => res.json());
     }
 }
