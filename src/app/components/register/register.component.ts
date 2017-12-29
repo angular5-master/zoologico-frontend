cad1ffad84521fd3411691feb86baa334034d27e
albracu <yo@alfredobravocuero.co>
2017-12-29 09:50:18 -0500

    Recibir los datos del formulario de Registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 
 @Component({
@@ -9,16 +10,22 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class RegisterComponent implements OnInit {
   public title: string;
+  public user: User;
 
   constructor(
       private _route: ActivatedRoute,
       private _router: Router
   ){
       this.title = 'Registro';
+      this.user = new User('', '', '', '', '','ROLE_USER', '' );
   }
 
   ngOnInit(){
       console.log('register.component cargado!!');
+  }
+
+  onSubmit(){
+      console.log(this.user);
   }
 
 }
